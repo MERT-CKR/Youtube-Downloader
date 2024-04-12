7bb9df6f00d54aa33f3e30cc535d1b22a019165c
Mert Çakır <82875825+MERT-CKR@users.noreply.github.com>
2024-04-12 21:05:34 +0300

    Add files via upload

--- YT Downloader.py
+++ YT Downloader.py
@@ -1,24 +1,47 @@
-from pytube import YouTube 
-import os 
+import pytube
+import moviepy.editor as mp
+import os
 
-# Find the desktop automatically and format it as an escape character
-desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
-desktop_path = desktop_path.replace("\\", r"\\")
 
-# url input from user 
-yt = YouTube( 
-    str(input("Enter the URL of the video you want to download: \n>> "))) 
-  
-# extract only audio 
-video = yt.streams.filter(only_audio=False).first() 
-  
-# download the file 
-out_file = video.download(output_path = desktop_path) 
-  
-# save the file 
-base, ext = os.path.splitext(out_file) 
-new_file = base + '.mp3'
-os.rename(out_file, new_file) 
-  
-# result of success 
-print(yt.title + " has been successfully downloaded.")
+from pytube import YouTube
+from moviepy.editor import AudioFileClip
+
+def youtube_to_mp3(url, filename):
+    # Masaüstü yolunu al
+    desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+    video_filename = os.path.join(desktop_path, filename + ".mp4")  # .mp4 uzantısını ekleyin
+    video.download(filename=video_filename)
+
+    # Ses dosyasını MP3 formatına dönüştür
+    audio = AudioFileClip(video_filename)
+    audio_filename = os.path.join(desktop_path, filename + ".mp3")
+    audio.write_audiofile(audio_filename)
+
+    # Orijinal dosyayı sil (isteğe bağlı)
+    os.remove(video_filename)
+
+# Kullanım örneği:
+
+
+def youtube_to_mp4(url, filename):
+  # Masaüstü yolunu al
+  desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+
+  yt = pytube.YouTube(url)
+  video = yt.streams.first()
+  video.download(filename=os.path.join(desktop_path, filename))
+
+def main():
+    selection = int(input("mp3 için: 0\nmp4 için: 1 \n>> "))
+    if selection !=1 and selection !=0:
+      main()
+    elif selection==0:
+        link = input("İndirmek istediğiniz MP3 dosyasının Youtube linkini girin: \n>> ")
+        youtube_to_mp3(link, "music")
+    elif selection==1:
+        link = input("İndirmek istediğiniz MP4 dosyasının Youtube linkini girin: \n>> ")
+        youtube_to_mp4(link, "video.mp4")
+main()
